ee3339f3aaf40dfacf1614e58a43920ae9e465ad
Ruslan <ruslansharipov1992@yandex.ru>
2022-07-12 17:18:18 +0500

    working header

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+//require_once($_SERVER['DOCEMENT_ROOT']. 'include.php');
+
+require_once($_SERVER['DOCEMENT_ROOT']. 'header_session_start.php');
+
+require_once($_SERVER['DOCEMENT_ROOT']. 'layout.php');
+
+
+
---
+++ layout.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title></title>
+    <link rel="icon" href="src/images/logo.png">
+    <link rel="stylesheet" href="src/bootstrap/css/bootstrap.css">
+    <link rel="stylesheet" href="src/css/main.css">
+</head>
+<body>
+<section class="header">
+<nav class="navbar navbar-expand-lg">
+    <div class="container">
+        <a class="navbar-brand" href="#">
+            <img class="img-fluid" src="src/images/logo_header.png" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Переключатель навигации">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+            <ul class="navbar-nav mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">КТО МЫ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">ПОЧЕМУ МЫ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">УСЛУГИ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">ЭТАПЫ РАБОТЫ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">КОНТАКТЫ</a>
+                </li>
+            </ul>
+            <button class="btn btn-my-danger">Оставить заявку</button>
+        </div>
+    </div>
+</nav>
+
+
+</section>
+
+
+<script src="src/bootstrap/js/bootstrap.js"></script>
+</body>
+</html>
