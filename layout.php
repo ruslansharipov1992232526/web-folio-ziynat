52e88564fe085d0a16935e6e891d36c9618820a2
Ruslan <ruslansharipov1992@yandex.ru>
2022-07-13 18:00:00 +0500

    добавлено 2 блока

--- layout.php
+++ layout.php
@@ -12,41 +12,220 @@
 </head>
 <body>
 <section class="header">
-<nav class="navbar navbar-expand-lg">
+    <nav class="navbar navbar-expand-lg">
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img class="img-fluid" src="src/images/logo_header.png" alt="">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Переключатель навигации">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                <ul class="navbar-nav mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">КТО МЫ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">ПОЧЕМУ МЫ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">УСЛУГИ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">ЭТАПЫ РАБОТЫ</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">КОНТАКТЫ</a>
+                    </li>
+                </ul>
+                <button class="btn btn-my-danger">Оставить заявку</button>
+            </div>
+        </div>
+    </nav>
     <div class="container">
-        <a class="navbar-brand" href="#">
-            <img class="img-fluid" src="src/images/logo_header.png" alt="">
-        </a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Переключатель навигации">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
-            <ul class="navbar-nav mb-2 mb-lg-0">
-                <li class="nav-item">
-                    <a class="nav-link" href="#">КТО МЫ</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">ПОЧЕМУ МЫ</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">УСЛУГИ</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">ЭТАПЫ РАБОТЫ</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">КОНТАКТЫ</a>
-                </li>
-            </ul>
-            <button class="btn btn-my-danger">Оставить заявку</button>
+        <div class="row">
+            <div class="d-flex">
+                <div class="col-lg-6 mx-lg-5 header_text">
+                    <p>Мы делали рекламу с 2007, когда это еще не было мейнстримом</p>
+                    <p>Теперь мы помогаем клиентам </p>
+                    <h1>узнать и полюбить <br> ваш бренд и в digital</h1>
+                </div>
+                <div class="col-lg-6 mx-lg-5 header_text">
+                    <!--                <video preload="auto" loop autoplay src="src/images/1.mp4"></video>-->
+                </div>
+            </div>
         </div>
     </div>
-</nav>
-
 
 </section>
+<section class="about" id="about">
+    <div class="container">
+        <div class="row">
+            <div class="col-my-6 d-flex">
+                <img class="img__about__one" src="src/images/about_one.png" alt="">
+                <img class="img__about__two" src="src/images/about_two.png" alt="">
+            </div>
+            <div class="col-my-6 d-grid">
+                <div class="text__about">
+                    <h1 class="about__title">Кто мы</h1>
+                    <p class="about__text">Команда профессиональных, увлеченных, гибких «think digital»
+                        специалистов в рамках большой компании Ziynat Design.
+                    </p>
+                </div>
+                <div class="text__about__and__icon mt-lg-3">
+                    <div class="d-grid">
+                        <div class="d-flex">
+                            <img src="src/images/about_three.png" alt="" class="img-fluid">
+                            <div class="d-grid">
+                                <h1 class="about__title__icon__two">Наша цель</h1>
+                                <p class="about__text__icon__two">
+                                    Рассказать о товаре или услуге на цифровых площадках с максимальной выгодой для вас.
+                                </p>
+                            </div>
+                        </div>
 
+                        <div class="d-flex">
+                            <img src="src/images/about_four.png" alt="" class="img-fluid">
+                            <div class="d-grid">
+                                <h1 class="about__title__icon__two">Уникальность предложения </h1>
+                                <p class="about__text__icon__two">
+                                    В умении подстроиться под ваши нужны, необычные пожелания и
+                                    найти технические решение даже тогда, когда это кажется невозможным.
+                                </p>
+                            </div>
+                        </div>
+                        <img class="about__ellipce" src="src/images/ellipse.png" alt="">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="why" id="why">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 d-grid justify-content-center">
+                <h1 class="why__title">Почему работать с нами комфортно? </h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="d-flex justify-content-center">
+                <div class="why__block d-grid justify-content-center text-center">
+                    <img src="src/images/why_one.png" class="img-fluid why__img" alt="">
+                    <p class="why__title__two">Индивидуальный подход</p>
+                    <p class="why__text">Не важно, разбираетесь вы в digital или впервые сталкиваетесь с цифровыми
+                        каналами продвижения – мы сможем выстроить с вами диалог и объяснить, почему нужно делать так
+                        или иначе. </p>
+                </div>
+                <div class="why__block d-grid justify-content-center text-center">
+                    <img src="src/images/why_two.png" class="img-fluid why__img" alt="">
+                    <p class="why__title__two">Не говорим «нет», говорим - «попробуем»</p>
+                    <p class="why__text">
+                        Даже для самого сложного технического вопроса мы попытаемся найти решение.
+                        <br><br><br><br><br>
+                    </p>
+                </div>
+                <div class="why__block d-grid justify-content-center text-center">
+                    <img src="src/images/why_three.png" class="img-fluid why__img" alt="">
+                    <p class="why__title__two">Соблюдаем договоренности </p>
+                    <p class="why__text">
+                        Мы строго относимся к своим обязательствам, потому что один довольный клиент
+                        приведет нам десяток новых.
+                        <br><br><br><br><br>
+                    </p>
+                </div>
+                <div class="why__block d-grid justify-content-center text-center">
+                    <img src="src/images/why_four.png" class="img-fluid why__img" alt="">
+                    <p class="why__title__two">Компетентность команды</p>
+                    <p class="why__text">
+                        Нам важно сделать работу хорошо, а это возможно только с профессионалами
+                        <br><br><br><br><br>
+                    </p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="services" id="services">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 d-grid justify-content-center">
+                <h1 class="services__title">Услуги</h1>
+                <p class="services__text">
+                    Мы предлагаем индивидуальные digital-решения, если вы точно знаете, чего хотите
+                    или, наоборот, абсолютно не понимаете, с чего начать.
+                </p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 d-flex">
+                <div class="col-4 d-flex ser__block__blur">
+                    <img class="img-fluid services__img" src="src/images/icon_servis/ser__icon1.png" alt="">
+                    <p class="services__text__two">Маркетинговые консультации и аудит бизнеса</p>
+                </div>
+                <div class="col-4 d-flex ser__block__blur">
+                    <img class="img-fluid services__img" src="src/images/icon_servis/ser__icon2.png" alt="">
+                    <p class="services__text__two">Создание и раскрутка личных брендов</p>
+                </div>
+                <div class="col-4 d-flex ser__block__blur">
+                    <img class="img-fluid services__img" src="src/images/icon_servis/ser__icon3.png" alt="">
+                    <p class="services__text__two">Разработка digital-стратегии и запуск брендов </p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 d-flex">
+                <div class="col-4 d-flex ser__block__blur">
+                    <img class="img-fluid services__img" src="src/images/icon_servis/ser__icon4.png" alt="">
+                    <p class="services__text__two">Разработка и техническая поддержка сайтов</p>
+                    <a href="">Заполнить бриф</a>
+                </div>
+                <div class="col-4 d-flex ser__block__blur">
+                    <img class="img-fluid services__img" src="src/images/icon_servis/ser__icon5.png" alt="">
+                    <p class="services__text__two">SEO-оптимизация сайтов для Google и Яндекс</p>
+                </div>
+                <div class="col-4 d-flex ser__block__blur">
+                    <img class="img-fluid services__img" src="src/images/icon_servis/ser__icon6.png" alt="">
+                    <p class="services__text__two">Контекстная реклама</p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 d-flex">
+                <div class="col-4 d-flex ser__block__blur">
+                    <img class="img-fluid services__img" src="src/images/icon_servis/ser__icon7.png" alt="">
+                    <p class="services__text__two">Digital PR</p>
+                </div>
+                <div class="col-4 d-flex ser__block__blur">
+                    <img class="img-fluid services__img" src="src/images/icon_servis/ser__icon8.png" alt="">
+                    <p class="services__text__two">SMM</p>
+                </div>
+                <div class="col-4 d-flex ser__block__blur">
+                    <img class="img-fluid services__img" src="src/images/icon_servis/ser__icon9.png" alt="">
+                    <p class="services__text__two">Копирайтинг </p>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-lg-12 d-flex">
+                <div class="col-4 d-flex ser__block__blur">
+                    <img class="img-fluid services__img" src="src/images/icon_servis/ser__icon10.png" alt="">
+                    <p class="services__text__two">Таргетинг</p>
+                </div>
+                <div class="col-4 d-flex ser__block__blur">
+                    <img class="img-fluid services__img" src="src/images/icon_servis/ser__icon11.png" alt="">
+                    <p class="services__text__two">Digital-дизайн </p>
+                </div>
+                <div class="col-4 d-flex ser__block__blur">
+                    <img class="img-fluid services__img" src="src/images/icon_servis/ser__icon12.png" alt="">
+                    <p class="services__text__two">Фото и видеопродакшн</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 <script src="src/bootstrap/js/bootstrap.js"></script>
 </body>
