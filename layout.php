f99bb412fd831a5330d11ecdc92d2774ffeb9ce0
Ruslan <ruslansharipov1992@yandex.ru>
2022-07-14 17:44:50 +0500

    готова версия под десктоп, осталось:

--- layout.php
+++ layout.php
@@ -9,10 +9,11 @@
     <link rel="icon" href="src/images/logo.png">
     <link rel="stylesheet" href="src/bootstrap/css/bootstrap.css">
     <link rel="stylesheet" href="src/css/main.css">
+    <link rel="stylesheet" href="src/css/owl.carousel.min.css" />
 </head>
 <body>
 <section class="header">
-    <nav class="navbar navbar-expand-lg">
+    <nav class="navbar navbar-expand-lg fixed-top">
         <div class="container">
             <a class="navbar-brand" href="#">
                 <img class="img-fluid" src="src/images/logo_header.png" alt="">
@@ -25,19 +26,19 @@
             <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                 <ul class="navbar-nav mb-2 mb-lg-0">
                     <li class="nav-item">
-                        <a class="nav-link" href="#">КТО МЫ</a>
+                        <a class="nav-link" href="#about">КТО МЫ</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">ПОЧЕМУ МЫ</a>
+                        <a class="nav-link" href="#why">ПОЧЕМУ МЫ</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">УСЛУГИ</a>
+                        <a class="nav-link" href="#services">УСЛУГИ</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">ЭТАПЫ РАБОТЫ</a>
+                        <a class="nav-link" href="#etaps">ЭТАПЫ РАБОТЫ</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link" href="#">КОНТАКТЫ</a>
+                        <a class="nav-link" href="#contact">КОНТАКТЫ</a>
                     </li>
                 </ul>
                 <button class="btn btn-my-danger">Оставить заявку</button>
@@ -46,14 +47,24 @@
     </nav>
     <div class="container">
         <div class="row">
-            <div class="d-flex">
-                <div class="col-lg-6 mx-lg-5 header_text">
-                    <p>Мы делали рекламу с 2007, когда это еще не было мейнстримом</p>
-                    <p>Теперь мы помогаем клиентам </p>
-                    <h1>узнать и полюбить <br> ваш бренд и в digital</h1>
+            <div class="d-flex aa">
+                <img class="ver__logo" src="src/images/ver__logo.png" alt="">
+                <div class="col-sm-7 col-lg-8 px-5 px-sm-5 header__info__text">
+                    <div class="overflow-hidden px-sm-5">
+                        <p class="header__text__one">Мы делали рекламу с 2007, когда это еще не было мейнстримом</p>
+                    </div>
+                    <div class="overflow-hidden px-sm-5">
+                        <p class="header__text__two">Теперь мы помогаем клиентам </p>
+                    </div>
+                    <div class="overflow-hidden px-sm-5">
+                        <p class="header__text__three">узнать и полюбить <br> ваш бренд и в digital</p>
+                    </div>
+                    <div class="overflow-hidden px-sm-5">
+                        <button class="btn btn-my-danger__three">Обсудить детали</button>
+                    </div>
                 </div>
-                <div class="col-lg-6 mx-lg-5 header_text">
-                    <!--                <video preload="auto" loop autoplay src="src/images/1.mp4"></video>-->
+                <div class="col-lg-4 header_text">
+                     <video preload="auto" loop autoplay src="src/images/1.mp4"></video>
                 </div>
             </div>
         </div>
@@ -179,8 +190,10 @@
             <div class="col-lg-12 d-flex">
                 <div class="col-4 d-flex ser__block__blur">
                     <img class="img-fluid services__img" src="src/images/icon_servis/ser__icon4.png" alt="">
-                    <p class="services__text__two">Разработка и техническая поддержка сайтов</p>
-                    <a href="">Заполнить бриф</a>
+                    <div class="d-grid">
+                        <p class="services__text__two">Разработка и техническая поддержка сайтов</p>
+                        <a class="services__link__briff" href="#">Заполнить бриф</a>
+                    </div>
                 </div>
                 <div class="col-4 d-flex ser__block__blur">
                     <img class="img-fluid services__img" src="src/images/icon_servis/ser__icon5.png" alt="">
@@ -226,7 +239,284 @@
         </div>
     </div>
 </section>
+<section class="etaps" id="etaps">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 d-grid justify-content-center">
+                <h1 class="etaps__title">Как начать с нами работать</h1>
+            </div>
+        </div>
+        <div class="row">
+            <div class="d-flex">
+                <img class="img-fluid arrow__etaps" src="src/images/left.png" alt="">
+                <div class="d-flex">
+                    <p class="one">1</p>
+                    <p class="two">2</p>
+                    <p class="three">3</p>
+                    <p class="four">4</p>
+                </div>
+                <div class="linea"></div>
+                <img class="img-fluid arrow__etaps" src="src/images/right.png" alt="">
+            </div>
+        </div>
+        <div class="row">
+            <div class="d-flex mt-5">
+
+                    <div class="text-center">
+                        <p class="text1">Знакомство и аудит</p>
+                    </div>
+                    <div class="text-center">
+                        <p class="text1">Коммерческое предложение</p>
+                    </div>
+                    <div class="text-center">
+                        <p class="text1">Начало работы </p>
+                    </div>
+                    <div class="text-center">
+                        <p class="text1">Оценка качества </p>
+                    </div>
+
+
+
+
+
+
+            </div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-lg-12 etps__bg">
+                <div class="d-flex">
+                    <div class="etaps__img">
+                        <img src="src/images/1.png" alt="">
+                    </div>
+                    <div class="d-grid etaps__block__right">
+                        <p class="etaps__text">
+                            Звоните, пишите, оставляйте заявку или заполняйте бриф.
+                            <br><br>
+                            Мы детально обсудим с вами текущую ситуацию вашей компании и изучим запрос.
+                        </p>
+                        <form class="form__my" action="">
+                            <div class="">
+                                <input type="name" class="form-control" id="inputEmail4" placeholder="+998 ">
+
+                                    <button class="btn btn-my-danger__input">Оставить заявку</button>
+
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-12 etps__bg d-none">
+                <div class="d-flex">
+                    <div class="etaps__img">
+                        <img src="src/images/2.png" alt="">
+                    </div>
+                    <div class="d-grid etaps__block__right">
+                        <p class="etaps__text">
+                            На основе первичных данных мы подготовим и презентуем индивидуальное коммерческое предложение.
+                            <br><br>
+                            Обсуждаем сроки, формы отчетности, прикрепляем к вам менеджера, с которым вы всегда на связи.
+                        </p>
+                        <form class="form__my" action="">
+                            <div class="">
+                                <input type="name" class="form-control" id="inputEmail4" placeholder="+998 ">
+
+                                <button class="btn btn-my-danger__input">Оставить заявку</button>
+
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-12 etps__bg d-none">
+                <div class="d-flex">
+                    <div class="etaps__img">
+                        <img src="src/images/3.png" alt="">
+                    </div>
+                    <div class="d-grid etaps__block__right">
+                        <p class="etaps__text">
+                            Мы заключаем договор, формируем команду под ваш запрос и начинаем работу над проектом.
+                        </p>
+                        <form class="form__my" action="">
+                            <div class="">
+                                <input type="name" class="form-control" id="inputEmail4" placeholder="+998 ">
+
+                                <button class="btn btn-my-danger__input">Оставить заявку</button>
+
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-lg-12 etps__bg d-none">
+                <div class="d-flex">
+                    <div class="etaps__img">
+                        <img src="src/images/4.png" alt="">
+                    </div>
+                    <div class="d-grid etaps__block__right">
+                        <p class="etaps__text">
+                            Мы всегда на связи в рабочее время, а технические специалисты устранят любые форс-мажорные неполадки в течение нескольких минут.
+                            <br><br>
+                            Отслеживать результативность работы и корректировать цели
+                            вы сможете вместе с менеджером проекта на протяжении всего времени сотрудничества.
+                        </p>
+                        <form class="form__my" action="">
+                            <div class="">
+                                <input type="name" class="form-control" id="inputEmail4" placeholder="+998 ">
+
+                                <button class="btn btn-my-danger__input">Оставить заявку</button>
+
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="trust " id="trust ">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 d-grid justify-content-center">
+                <h1 class="trust__title">Нам доверяют</h1>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-12 d-flex justify-content-center">
+                <div class="owl-carousel owl-theme mt-lg-5">
+                    <div class="carousel__block"><img class="carousel__block__img img-fluid" src="src/images/logobrand/1.png" alt=""></div>
+                    <div class="carousel__block"><img class="carousel__block__img img-fluid" src="src/images/logobrand/2.png" alt=""></div>
+                    <div class="carousel__block"><img class="carousel__block__img img-fluid" src="src/images/logobrand/3.png" alt=""></div>
+                    <div class="carousel__block"><img class="carousel__block__img img-fluid" src="src/images/logobrand/4.png" alt=""></div>
+                    <div class="carousel__block"><img class="carousel__block__img img-fluid" src="src/images/logobrand/6.png" alt=""></div>
+                    <div class="carousel__block"><img class="carousel__block__img img-fluid" src="src/images/logobrand/9.png" alt=""></div>
+                    <div class="carousel__block"><img class="carousel__block__img img-fluid" src="src/images/logobrand/10.png" alt=""></div>
+                    <div class="carousel__block"><img class="carousel__block__img img-fluid" src="src/images/logobrand/11.png" alt=""></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="contact" id="contact">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6">
+                <h1 class="contact__title">Контакты</h1>
+                <p class="contact__text__top__one">Начнем знакомство?</p>
+                <p class="contact__text__top__two">Для обсуждения и заказа услуг достаточно оставить заявку.
+                    Наш менеджер выйдет на связь, а специалисты разработают индивидуальное коммерческое предложение.
+                </p>
+                <form class="row g-3">
+                    <div class="col-md-6">
+                        <input type="name" class="form-control" id="inputEmail4" placeholder="John Do">
+                    </div>
+                    <div class="col-md-6">
+                        <input type="phone" class="form-control" id="inputPassword4" placeholder="Номер телефона">
+                    </div>
+                    <div class="col-12">
+                        <input type="text" class="form-control" id="inputAddress" placeholder="Сообщение">
+                    </div>
+                    <div class="col-12">
+                        <button class="btn btn-my-danger__two">Оставить заявку</button>
+                    </div>
+                </form>
+                <hr class="line">
+                <div class="d-flex contacts">
+                    <div class="contact__left__block d-grid">
+                        <div class="d-flex contact__info__one">
+                            <img class="contact__img__one" src="src/images/social/ph.png" alt="">
+                            <p class="contact__text">+998 78 150-20-50</p>
+                        </div>
+                        <div class="d-flex contact__info__two">
+                            <img class="contact__img__one" src="src/images/social/tg.png" alt="">
+                            <p class="contact__text">+998 90 345-20-50</p>
+                        </div>
+                    </div>
+                    <div class="contact__rigt__block d-flex">
+                        <div class="contact__left__block d-grid">
+                            <div class="d-flex contact__info__one">
+                                <img class="contact__img__one" src="src/images/social/ph.png" alt="">
+                                <p class="contact__text">+998 78 150-20-50</p>
+                            </div>
+                            <div class="d-flex contact__info__two">
+                                <img class="contact__img__one" src="src/images/social/tg.png" alt="">
+                                <p class="contact__text">+998 90 345-20-50</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-6">
+                <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A9e7313b77909ffd157bca8e47dbda4c364915ea6dc8c7d8d4c5f3ded6bc667be&amp;source=constructor" width="930" height="800" frameborder="0"></iframe>
+            </div>
+
+
+
+        </div>
+    </div>
+</section>
+<footer class="footer" id="footer">
+        <div class="container">
+            <div class="row">
+            <div class="col-lg-6">
+                <img class="img-fluid footer__logo" src="src/images/footer__logo.png" alt="">
+            </div>
+
+            <div class="col-lg-6 d-flex justify-content-end">
+                <a href=""><img class="img-fluid" src="src/images/fotsoc1.png" alt=""></a>
+                <a href=""><img class="img-fluid" src="src/images/fotsoc2.png" alt=""></a>
+                <a href=""><img class="img-fluid" src="src/images/fotsoc3.png" alt=""></a>
+            </div>
+        </div>
+
+    </div>
+    <hr class="footer__hr">
+    <div class="container">
+        <div class="row">
+            <p class="footer__text">Copyright © Ziynat Design • 2022 • All rights reserved</p>
+        </div>
+    </div>
+</footer>
 
 <script src="src/bootstrap/js/bootstrap.js"></script>
+<script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+<script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+<script src="src/css/owl.carousel.min.js"></script>
+<script>
+    $(document).ready(function(){
+        const slider = $(".owl-carousel").owlCarousel({
+            loop:true,
+            margin:10,
+            nav:true,
+            // autoplay: true,
+            navText: ['<span aria-label="Previous"></span>','<span aria-label="Next"></span>'],
+            responsive:{
+                0:{
+                    items:1
+                },
+                600:{
+                    items:3
+                },
+                1000:{
+                    items:5
+                }
+            }
+        });
+    });
+</script>
+<script>
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if(scroll < 300){
+            $('.fixed-top').css('background', 'transparent');
+        } else{
+            $('.fixed-top').css('background', 'rgba(255, 255, 255, 1)');
+        }
+    });
+</script>
 </body>
 </html>
